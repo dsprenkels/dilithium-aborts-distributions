baec9a0da69a37ab7db0f3791cb3ef0b7803880f
Daan Sprenkels <daan@dsprenkels.com>
2021-06-25 13:44:51 +0200

    Fix feedback from Bas

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,25 @@
 use std::collections::BTreeMap;
 
-const BETA: i32 = 2;
-const GAMMA1: i32 = 4;
+const BETA: i64 = 2;
+const GAMMA1: i64 = 7;
+const ORD_B: i64 = 2 * BETA - 1;
+const ORD_Y: i64 = 2 * GAMMA1;
 
-type CounterMap = BTreeMap<(i32, i32, i32), i32>;
+type CounterMap = BTreeMap<(i64, i64, i64), i64>;
 
-fn z_is_in_bounds(z: i32) -> bool {
+fn z_is_in_bounds(z: i64) -> bool {
     z.abs() < GAMMA1 - BETA
 }
 
-fn dilithium_ztrick(y1: i32, y2: i32, y1p: i32) -> CounterMap {
+fn dilithium_ztrick(y1: i64, y2: i64, y1p: i64) -> CounterMap {
     assert!(-GAMMA1 < y1 && y1 <= GAMMA1);
     assert!(-GAMMA1 < y2 && y2 <= GAMMA1);
     assert!(-GAMMA1 < y1p && y1p <= GAMMA1);
 
     let mut zc = BTreeMap::new();
-    let mut count_signature = |z1, z2, c| {
+    let mut count_signature = |z1, z2, c, v| {
         let counter = zc.entry((z1, z2, c)).or_default();
-        *counter += 1;
+        *counter += v;
     };
 
     // Iterate over h.  H is a tuple of two mappings from Y to B, because
@@ -25,46 +27,45 @@ fn dilithium_ztrick(y1: i32, y2: i32, y1p: i32) -> CounterMap {
     // We already know the values of the inputs, they are (y1, y2) and
     // (y1p, y2), so we only need to iterate over two challenge values.
     for (c, cp) in itertools::iproduct!(-BETA + 1..BETA, -BETA + 1..BETA) {
+        let mut abort = false;
         let mut z1 = y1 + c;
         let mut z2 = y2 + c;
         if !z_is_in_bounds(z1) {
-            // Sample y1 again.
-            if (y1, y2) == (y1p, y2) {
-                // Hash function has to return the same challenge.
-                z1 = y1p + c;
-            } else {
-                // Hash function just maps to a new random value.
-                z1 = y1p + cp;
-                z2 = y2 + cp;
-            }
-
-            // z-check from the second iteration.
-            if z_is_in_bounds(z1) && z_is_in_bounds(z2) {
-                // Good signature in two iterations.  Ouput.
-                count_signature(z1, z2, cp);
+            z1 = y1p + cp;
+            z2 = y2 + cp;
+            if !z_is_in_bounds(z1) || !z_is_in_bounds(z2) {
+                abort = true;
             }
         } else if !z_is_in_bounds(z2) {
-            // Abort completely.
+            abort = true;
+        }
+
+        if abort {
+            continue;
+        } else if (y1, y2) != (y1p, y2) {
+            count_signature(z1, z2, cp, ORD_B.pow(ORD_Y as u32 - 2));
+        } else if (y1, y2) == (y1p, y2) && c == cp {
+            count_signature(z1, z2, cp, ORD_B.pow(ORD_Y as u32 - 1));
+            continue;
+        } else if (y1, y2) == (y1p, y2) && c != cp {
             continue;
         } else {
-            // Good singature in one iteration.  Output.
-            count_signature(z1, z2, c);
-            continue;
+            unreachable!();
         }
     }
 
-    return zc;
+    zc
 }
 
-fn dilithium_vanilla(y1: i32, y2: i32, y1p: i32) -> CounterMap {
+fn dilithium_vanilla(y1: i64, y2: i64, y1p: i64) -> CounterMap {
     assert!(-GAMMA1 < y1 && y1 <= GAMMA1);
     assert!(-GAMMA1 < y2 && y2 <= GAMMA1);
     assert!(-GAMMA1 < y1p && y1p <= GAMMA1);
 
     let mut zc = BTreeMap::new();
-    let mut count_signature = |z1, z2, c| {
+    let mut count_signature = |z1, z2, c, v| {
         let counter = zc.entry((z1, z2, c)).or_default();
-        *counter += 1;
+        *counter += v;
     };
 
     // Iterate over h.  H is a tuple of two mappings from Y to B, because
@@ -72,18 +73,30 @@ fn dilithium_vanilla(y1: i32, y2: i32, y1p: i32) -> CounterMap {
     // We already know the values of the inputs, they are (y1, y2) and
     // (y1p, y2), so we only need to iterate over two challenge values.
     for (c, cp) in itertools::iproduct!(-BETA + 1..BETA, -BETA + 1..BETA) {
+        let mut abort = false;
         let z1 = y1 + c;
         let z2 = y2 + c;
         let _z1p = y1p + cp;
         let _z2p = y2 + cp;
-        if z_is_in_bounds(z1) && z_is_in_bounds(z2) {
-            // Good singature in one iteration.  Output.
-            count_signature(z1, z2, c);
+        if !z_is_in_bounds(z1) || !z_is_in_bounds(z2) {
+            abort = true;
+        }
+
+        if abort {
             continue;
+        } else if (y1, y2) != (y1p, y2) {
+            count_signature(z1, z2, cp, ORD_B.pow(ORD_Y as u32 - 2));
+        } else if (y1, y2) == (y1p, y2) && c == cp {
+            count_signature(z1, z2, cp, ORD_B.pow(ORD_Y as u32 - 1));
+            continue;
+        } else if (y1, y2) == (y1p, y2) && c != cp {
+            continue;
+        } else {
+            unreachable!();
         }
     }
 
-    return zc;
+    zc
 }
 
 fn is_uniform(map: &CounterMap) -> bool {
@@ -109,7 +122,7 @@ fn merge_results(map: &mut CounterMap, other: &CounterMap) {
 }
 
 fn main() {
-    let mut vanilla_results = BTreeMap::new();
+    let mut vanilla_results = BTreeMap::<_, i64>::new();
     for (y1, y2, y1p) in itertools::iproduct!(
         (-GAMMA1 + 1..=GAMMA1),
         (-GAMMA1 + 1..=GAMMA1),
@@ -126,6 +139,8 @@ fn main() {
     ) {
         merge_results(&mut ztrick_results, &dilithium_ztrick(y1, y2, y1p));
     }
+
+    dbg!(&ztrick_results);
 
     dbg!(GAMMA1, BETA);
 
